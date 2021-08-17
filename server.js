b8664b5a960f301c05fc1036cb34db61382080be
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-17 12:49:47 +0530

    Reached to the boiler plate for the server part.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("server");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.json("server started.");
+})
+
+app.listen(port, () => {
+    console.log("server listning to the port " + port);
+})
