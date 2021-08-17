6843e3e551310a12aadec623baec3ca053a0d33c
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-17 13:02:17 +0530

    Implemented morgan.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const port = process.env.PORT || 4000;
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.json("server started.");
