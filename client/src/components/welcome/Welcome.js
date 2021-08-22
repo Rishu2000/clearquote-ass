df479ca59216b343180afcdd1a29a4579b8fef4d
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-22 18:50:39 +0530

    Logout functionality complete.

--- client/src/components/welcome/Welcome.js
+++ client/src/components/welcome/Welcome.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import axios from 'axios'
 
-const Welcome = () => {
+const Welcome = ({user, setUser}) => {
+
+const handleLogout = (e) => {
+    e.preventDefault();
+    axios.get('http://localhost:4000/logout')
+        .then((res) => {
+            setUser(null)
+            // console.log(res.data);
+        })
+}
+
     return (
         <div>
-            Welcome      
+            Welcome {user}   
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
         </div>
     )
 }
